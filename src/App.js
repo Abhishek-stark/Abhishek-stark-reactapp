d274010f932e4363c9625e552a832a8d32a23e94
Abhishek <starkop688@gmail.com>
2022-12-14 22:50:02 +0530

    Small changes in App.js with Scss testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.scss";
+// import logo from "./logo.svg";
+// import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App  appstyle">
+      <div className="form-background">
+        <form action="#" className="formmain">
+          <div className="formmain--email">
+            <input type="email" placeholder="Enter your email" />
+          </div>
+          <div className="formmain--password">
+            <input type="password" placeholder="Enter your password" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
