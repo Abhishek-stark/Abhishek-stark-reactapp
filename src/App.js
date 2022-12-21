40a5ec6da22dcf0aef0b0324f1104da0fe690bf4
Abhishek <starkop688@gmail.com>
2022-12-22 00:50:11 +0530

    initial style and fetching is added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,75 @@
-import { useEffect } from "react";
+import { useEffect, React, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./style.scss";
-// import logo from "./logo.svg";
-// import "./App.css";
 import axios from "axios";
+import Dashboard from "./component/Dashboard";
+import Login from "./component/Login";
+import Register from "./component/Register";
+import Useraccount from "./component/Useraccount";
+import Homepage from "./component/Homepage";
+import Tourpage from "./component/Tourpage";
 
-const getTour = async () => {
-  try {
-    const res = await axios.get("https://apifortour.onrender.com");
-    console.log(res.data);
-  } catch (err) {
-    console.log(err.message);
-  }
-};
+import Tourdetailpage from "./component/Tourdetailpage";
+import Passwordsetting from "./component/Passwordsetting";
 
-// useEffect(()=>{
-//   getTour();
-// })
+import ProtectedRouted from "./component/ProtectedRouted";
+import Nopage from "./component/Nopage";
+import AdminPage from "./component/AdminPage";
+axios.defaults.withCredentials = true;
 
+// https://apifortour.onrender.com/
 function App() {
   return (
     <div className="App  appstyle">
-      <div className="form-background">
-        <form action="#" className="formmain">
-          <div className="formmain--email">
-            <input type="email" placeholder="Enter your email" />
-            {getTour()}
-          </div>
-          <div className="formmain--password">
-            <input type="password" placeholder="Enter your password" />
-          </div>
-        </form>
-      </div>
+      <Router forceRefresh={true}>
+        <Homepage />
+        <div className="container">
+          <Routes>
+            <Route
+              path="/dashboard"
+              exact
+              element={<ProtectedRouted Component={Dashboard} />}
+              //  element={<Dashboard />}
+            />
+            <Route
+              path="/updatePassword"
+              exact
+              element={<ProtectedRouted Component={Passwordsetting} />}
+              // element={<Passwordsetting />}
+            />{" "}
+            <Route
+              path="/login"
+              exact
+              element={<ProtectedRouted Component={Login} />}
+            />
+            <Route
+              path="/admin"
+              exact
+              element={<ProtectedRouted Component={AdminPage} />}
+            />
+            <Route path="/" exact element={<Tourpage />} />
+            <Route
+              path="/useraccount"
+              exact
+              element={<ProtectedRouted Component={Useraccount} />}
+              // element={<Useraccount />}
+            />
+            <Route
+              path="/register"
+              exact
+              element={<Register />}
+              //  element={<Register />}
+            />
+            <Route
+              path="/tourdetail"
+              exact
+              element={<ProtectedRouted Component={Tourdetailpage} />}
+              // element={<Tourdetailpage />}
+            />
+            <Route path="/*" element={<Nopage />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
