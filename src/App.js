6a536cdc10b40ee4a39c7f285f96b002df2c7f46
Abhishek <starkop688@gmail.com>
2023-01-24 20:32:16 +0530

    updating match

--- src/App.js
+++ src/App.js
@@ -1,15 +1,75 @@
-import "./style.scss";
-import Names from "./components/Names";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { ToastContainer } from 'react-toastify'
+// import 'react-toastify/dist/ReactToastify.css'
+// import Header from './components/Header';
+import Dashboard from './component/Dashboard';
+import Login from './component/Login';
+import Register from './component/Register';
+import Useraccount from './component/Useraccount';
+import Homepage from './component/Homepage';
+import Tourpage from './component/Tourpage';
+import axios from 'axios';
+import Tourdetailpage from './component/Tourdetailpage';
+import Passwordsetting from './component/Passwordsetting';
 
-// axios.defaults.withCredentials = true;
+import ProtectedRouted from './component/ProtectedRouted';
+import Nopage from './component/Nopage';
+import AdminPage from './component/AdminPage';
+axios.defaults.withCredentials = true;
 
-// https://apifortour.onrender.com/
 function App() {
   return (
-    <div className="App">
-      <p>Lorem ipsum dolor sit amet consectetur adipisi</p>
-      <Names />
-    </div>
+    <>
+      <Router forceRefresh={true}>
+        <Homepage />
+        <div className="container">
+          <Routes>
+            <Route
+              path="/dashboard"
+              exact
+              element={<ProtectedRouted Component={Dashboard} />}
+              //  element={<Dashboard />}
+            />
+            <Route
+              path="/updatePassword"
+              exact
+              element={<ProtectedRouted Component={Passwordsetting} />}
+              // element={<Passwordsetting />}
+            />{' '}
+            <Route
+              path="/login"
+              exact
+              element={<ProtectedRouted Component={Login} />}
+            />
+            <Route
+              path="/admin"
+              exact
+              element={<ProtectedRouted Component={AdminPage} />}
+            />
+            <Route path="/" exact element={<Tourpage />} />
+            <Route
+              path="/useraccount"
+              exact
+              element={<ProtectedRouted Component={Useraccount} />}
+              // element={<Useraccount />}
+            />
+            <Route
+              path="/register"
+              exact
+              element={<Register />}
+              //  element={<Register />}
+            />
+            <Route
+              path="/tourdetail"
+              exact
+              element={<ProtectedRouted Component={Tourdetailpage} />}
+              // element={<Tourdetailpage />}
+            />
+            <Route path="/*" element={<Nopage />} />
+          </Routes>{' '}
+        </div>{' '}
+      </Router>{' '}
+    </>
   );
 }
 
