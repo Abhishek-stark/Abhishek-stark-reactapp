eeb0b86b2c48542dfa0fd665054c36cc9341ae88
Abhishek <starkop688@gmail.com>
2023-02-09 23:43:21 +0530

    adding withCredentials in axios

--- src/reducers/authService.js
+++ src/reducers/authService.js
@@ -1,21 +1,19 @@
-import axios from 'axios';
-import { loadStripe } from '@stripe/stripe-js';
+import axios from "axios";
+import { loadStripe } from "@stripe/stripe-js";
 const stripePromise = loadStripe(
-    'pk_test_51LhXM5SBrxf9grZuKlaBjNwd4LPft9O4c06bUKX8eBsgSBgcxaxMcm2jDFZZtTcRmVitjammnAkHhD19KLepU0ns00ZPAKOmmI'
+  "pk_test_51LhXM5SBrxf9grZuKlaBjNwd4LPft9O4c06bUKX8eBsgSBgcxaxMcm2jDFZZtTcRmVitjammnAkHhD19KLepU0ns00ZPAKOmmI"
 );
 
-const bookTour = async(tourId) => {
-    // 1) Get checkout session from API
-    const session = await axios.get(
-        `http://127.0.0.1:8000/api/v1/bookings/checkout-session/${tourId}`
-    );
-    // console.log(session);
-    // var url = session.data.url;
-    // navigate(`${url}`);
-    const stripe = await stripePromise;
-    await stripe.redirectToCheckout({
-        sessionId: session.data.session.id,
-    });
+const bookTour = async (tourId) => {
+  // 1) Get checkout session from API
+  const session = await axios.get(
+    `https://apifortour.onrender.com/api/v1/bookings/checkout-session/${tourId}`
+  );
+
+  const stripe = await stripePromise;
+  await stripe.redirectToCheckout({
+    sessionId: session.data.session.id,
+  });
 };
 
 //
@@ -23,126 +21,121 @@ const bookTour = async(tourId) => {
 // const API_URL = '/api/v1/users/';
 
 // Register user
-const register = async(userData) => {
-    const response = await axios.post(
-        'https://apifortour.onrender.com/api/v1/users/signup',
+const register = async (userData) => {
+  const response = await axios.post(
+    "https://apifortour.onrender.com/api/v1/users/signup",
 
-        userData
-    );
+    userData
+  );
 
-    if (response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data));
-    }
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
 
-    return response.data;
+  return response.data;
 };
 
-const update = async(userData) => {
-    const response = await axios.patch(
-        'https://apifortour.onrender.com/api/v1/users/updateMe',
-        userData
-    );
+const update = async (userData) => {
+  const response = await axios.patch(
+    "https://apifortour.onrender.com/api/v1/users/updateMe",
+    userData,
+    { withCredentials: true }
+  );
 
-    if (response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data));
-    }
-    return response.data;
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  return response.data;
 };
 
-const updatePassword = async(userData) => {
-    const response = await axios.patch(
-        'https://apifortour.onrender.com/api/v1/users/updateMyPassword',
-        userData
-    );
+const updatePassword = async (userData) => {
+  const response = await axios.patch(
+    "https://apifortour.onrender.com/api/v1/users/updateMyPassword",
+    userData,
+    { withCredentials: true }
+  );
 
-    if (response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data));
-    }
-    return response.data;
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  return response.data;
 };
 
 // Login user
-const login = async(userData) => {
-    const response = await axios.post(
-        'https://apifortour.onrender.com/api/v1/users/login',
-        userData
-    );
+const login = async (userData) => {
+  const response = await axios.post(
+    "https://apifortour.onrender.com/api/v1/users/login",
+    userData
+  );
 
-    if (response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data));
-    }
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
 
-    return response.data;
+  return response.data;
 };
 
 // Logout user
 // const logout = () => {
 //     localStorage.removeItem('user');
 // };
-const logout = async() => {
-    const res = await axios.get(
-        'https://apifortour.onrender.com/api/v1/users/logout'
-    );
-    if (res.data.status === 'success') {
-        // localStorage.removeItem('user');
-        // localStorage.removeItem('tours');
-        // localStorage.removeItem('tourdetail');
-        localStorage.clear();
-    }
-    return res;
-};
-// const getTour = async() => {
-//     const response = await axios.get('http://localhost:8000');
-//     if (response.data) {
-//         localStorage.setItem('tours', JSON.stringify(response.data));
-//     }
-//     return response.data;
-// };
-
-const getOneTour = async(slug) => {
-    const response = await axios.get(
-        'https://apifortour.onrender.com/tour' + slug
-    );
-    if (response.data) {
-        console.log(response.data);
-        localStorage.setItem('tourdetail', JSON.stringify(response.data));
-    }
-    return response.data;
-};
-const WriteReview = async(data, token) => {
-    const response = await axios.post(
-        'http://127.0.0.1:8000/api/v1/reviews',
-        data, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        }
-    );
-    if (response.data) {
-        console.log(response.data);
-    }
-    return response.data;
+const logout = async () => {
+  const res = await axios.get(
+    "https://apifortour.onrender.com/api/v1/users/logout"
+  );
+  if (res.data.status === "success") {
+    localStorage.clear();
+  }
+  return res;
 };
 
-const getAllusers = async() => {
-    const response = await axios.get('http://127.0.0.1:8000/api/v1/users');
-    if (response.data) {
-        console.log(response.data);
+const getOneTour = async (slug) => {
+  const response = await axios.get(
+    "https://apifortour.onrender.com/tour" + slug,
+    { withCredentials: true }
+  );
+  if (response.data) {
+    console.log(response.data);
+    localStorage.setItem("tourdetail", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+const WriteReview = async (data, token) => {
+  const response = await axios.post(
+    "http://127.0.0.1:8000/api/v1/reviews",
+    data,
+    { withCredentials: true },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     }
-    return response.data;
+  );
+  if (response.data) {
+    console.log(response.data);
+  }
+  return response.data;
+};
+
+const getAllusers = async () => {
+  const response = await axios.get("http://127.0.0.1:8000/api/v1/users");
+  if (response.data) {
+    console.log(response.data);
+  }
+  return response.data;
 };
 
 const authService = {
-    register,
-    logout,
-    login,
-    update,
-    // getTour,
-    // bookTour,
-    getOneTour,
-    updatePassword,
-    WriteReview,
-    getAllusers,
+  register,
+  logout,
+  login,
+  update,
+  // getTour,
+  // bookTour,
+  getOneTour,
+  updatePassword,
+  WriteReview,
+  getAllusers,
 };
 
 export default authService;
